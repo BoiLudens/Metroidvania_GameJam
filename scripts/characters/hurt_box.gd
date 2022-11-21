7c1e6b55ce633e22b0760f0352ee94a96bceb217
Matthew Anthony Coker <mattacoker@gmail.com>
2022-11-20 20:31:52 -0500

    reduced code and instead changed in editor

--- scripts/characters/hurt_box.gd
+++ scripts/characters/hurt_box.gd
@@ -1,12 +1,6 @@
 class_name HurtBox
 extends Area2D
 
-func _init() -> void:
-	# The hurtbox should detect hits but not deal them. This variable does that.
-	monitorable = false
-	# This turns off collision layer bit 1 and turns on bit 2. It's the physics layer we reserve to hurtboxes in this demo.
-	collision_mask = 2
-
 func _ready() -> void:
 	area_entered.connect(self._on_area_entered)
 
--- scripts/traps/hit_box.gd
+++ scripts/traps/hit_box.gd
@@ -3,12 +3,6 @@ extends Area2D
 
 var damage
 
-func _init() -> void:
-	collision_mask = 0
-	# This turns off collision mask bit 1 and turns on bit 2. It's the physics layer we reserve to hurtboxes in this demo.
-	collision_layer = 2
-# Called when the node enters the scene tree for the first time.
-
 func set_damage(new_damage):
 	damage = new_damage
 	
