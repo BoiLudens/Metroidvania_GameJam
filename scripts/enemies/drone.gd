263dc7945d583984f76ca213ccee7269afbcfc2a
Matthew Anthony Coker <mattacoker@gmail.com>
2022-11-22 22:03:41 -0500

    replaced hp bar and enemy dies

--- scripts/enemies/drone.gd
+++ scripts/enemies/drone.gd
@@ -1,19 +1,9 @@
-extends Sprite2D
+extends Enemy
 
-@onready var health_bar = $Health
-var health = 100 
+@export var health_max_override: float = 200
+@export var health_value_override: float = 200
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	health_bar.value = health
-	if (health <= 0):
-		print("death")
-
-func take_damage(damage):
-	health -= damage
-	print("damage taken")
+	health_value = health_value_override
+	health_max = health_value_override
+	set_health_bar(health_value)
