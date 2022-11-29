d4db1e464c579db575dddacffcdf125e61d8eb51
Synellx <synellx@gmail.com>
2022-11-29 10:54:30 -0500

    made a thing

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -1,6 +1,7 @@
 class_name Enemy extends CharacterBody2D
 
 @onready var health_bar = $Health
+@onready var death = $Death
 var health_max: float = 100
 var health_value: float = 100 
 
@@ -11,7 +12,9 @@ func take_damage(damage):
 	health_value -= damage
 	set_health_bar(health_value)
 	if (health_value <= 0):
+		death.play("death")
 		queue_free()
+		
 
 func set_health_bar(health):
 	health_bar.value = health_value/health_max * 100
