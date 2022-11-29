d8189ab7af0575def4bccd4defc91bcef1ccda6d
Matthew Anthony Coker <mattacoker@gmail.com>
2022-11-28 22:45:16 -0500

    back dash added (#77)
    * dash added
    
    * it is now an accidental backdash
    
    * can now dash even while moving
    
    Co-authored-by: Synellx <synellx@gmail.com>

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -6,8 +6,13 @@ extends CharacterBody2D
 @onready var animator = $AnimationPlayer
 @onready var hurt_box = $HurtBox
 
-const SPEED = 300.0
+const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
+
+#dash
+var dash_direction
+var can_dash = false
+var dashing = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -17,15 +22,15 @@ var whip_damage = 20
 signal set_health(health)
 
 func _ready():
-	emit_signal("set_health", health)
+	dash_direction = Vector2(-sprite_player.scale.x, 0)
 	sprite_weapon.get_node("HitBox").set_damage(whip_damage)
+	emit_signal("set_health", health)
 
-func _process(delta):
+
+func _physics_process(delta):
 	check_death()
 	if Input.is_action_just_pressed("action_attack"):
 		animator.play("Attack")
-
-func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -33,11 +38,12 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("action_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		can_dash = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
+	if direction and !dashing:
 		velocity.x = direction * SPEED
 		
 		if(velocity.x < 0):
@@ -49,6 +55,24 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	dash()
+	
+	#dash
+func dash():
+	if is_on_floor():
+		can_dash = true
+	
+	if Input.is_action_pressed("move_left"):
+		dash_direction = Vector2(1,0)
+	if Input.is_action_pressed("move_right"):
+		dash_direction = Vector2(-1,0)
+		
+	if Input.is_action_just_pressed("dash") and can_dash:
+		velocity = dash_direction.normalized() * 2500
+		can_dash = false
+		dashing = true
+		await(get_tree().create_timer(0.2).timeout)
+		dashing = false
 
 func check_death():
 	if (health <= 0):
