ab0573e637b7a4d392a8da4b02e6a1d8d379ca54
Matthew Anthony Coker <mattacoker@gmail.com>
2022-11-28 21:58:55 -0500

    replaced hp bar, enemy dies, and new enemy base class (#78)
    * replaced hp bar and enemy dies
    
    * enemy drone now follows a path (#79)
    
    * refactored to use the characterbody2d instead

---
+++ scripts/enemies/enemy.gd
@@ -0,0 +1,17 @@
+class_name Enemy extends CharacterBody2D
+
+@onready var health_bar = $Health
+var health_max: float = 100
+var health_value: float = 100 
+
+func _ready():
+	set_health_bar(health_value)
+
+func take_damage(damage):
+	health_value -= damage
+	set_health_bar(health_value)
+	if (health_value <= 0):
+		queue_free()
+
+func set_health_bar(health):
+	health_bar.value = health_value/health_max * 100
