7c3bce183f132c1ebbc3bbdb7f96bf4dac4d33c7
Matthew Anthony Coker <mattacoker@gmail.com>
2022-11-28 19:30:58 -0500

    updated layers names, and reassigned collisions, code for enemy

--- scripts/enemies/gas_mask.gd
+++ scripts/enemies/gas_mask.gd
@@ -1,65 +1,42 @@
 extends CharacterBody2D
 
+@export var starting_direction : Vector2 = Vector2(0,1)
+@export var idle_time : float = .8
+
+@onready var left_ray = $LeftRay
+@onready var timer = $Timer
+@onready var gas_mask_sprite = $GasMaskSprite
+
+const UP = Vector2.UP
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 var movement = Vector2()
 var speed = 50.0
 var start = 50
 var stop = 0
 var moving_left = true
-@export var starting_direction : Vector2 = Vector2(0,1)
-@export var idle_time : float = .8
-
-
-const UP = Vector2.UP
-@onready var animation_tree = $AnimationTree
-@onready var state_machine = animation_tree.get("parameters/playback")
-@onready var left_ray = $left_ray
-@onready var timer = $Timer
-@onready var walk = $walk
-@onready var idle = $idle
-
-
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-func _ready():
-	
-
-
-	animation_tree.set("parameters/walk/blend_position", starting_direction)
 
 func _physics_process(delta):
-
 	move()
 	
 	if left_ray.is_colliding():
-		
 		speed = stop
 		movement = move_and_slide()
-		walk.visible = not walk.visible
 		timer.start(idle_time)
 		moving_left = !moving_left
 		scale.x = -scale.x
-	
-		
-	
-	
-	
-
-	
-
+	if velocity.x != 0:
+		gas_mask_sprite.play("walk")
+	else:
+		gas_mask_sprite.play("idle")
 
 func move():
-	
 	velocity.y += gravity
 	velocity.x = speed if moving_left else -speed
 	movement = move_and_slide() 
 
-
-
 func _on_timer_timeout():
-	idle.visible = not idle.visible
 	speed = start
 	move()
-	
-	
