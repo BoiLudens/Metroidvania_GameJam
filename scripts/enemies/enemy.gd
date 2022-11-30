d4f7ecb3ccfffecbeabd547122b344da4a90d805
Synellx <117962355+Synellx@users.noreply.github.com>
2022-11-29 20:51:10 -0500

    40 enemy animation base death (#84)
    * made a thing
    
    * refactored a bit to enable death explosion
    
    Co-authored-by: Matthew Anthony Coker <mattacoker@gmail.com>

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -1,17 +1,21 @@
 class_name Enemy extends CharacterBody2D
 
 @onready var health_bar = $Health
+@onready var death = $Death
 var health_max: float = 100
 var health_value: float = 100 
 
 func _ready():
 	set_health_bar(health_value)
+	death.visible = false
 
 func take_damage(damage):
 	health_value -= damage
 	set_health_bar(health_value)
 	if (health_value <= 0):
-		queue_free()
+		death.visible = true
+		death.play("death")
+		
 
 func set_health_bar(health):
 	health_bar.value = health_value/health_max * 100
