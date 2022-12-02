6a0423b01a4512db5b2b8236d851fac2fe77e295
Matthew Anthony Coker <mattacoker@gmail.com>
2022-12-01 21:29:23 -0500

    enemy now shoots and doesnt die and switches states

--- scripts/enemies/drone.gd
+++ scripts/enemies/drone.gd
@@ -12,11 +12,12 @@ const LAZER_BOLT = preload("res://prefabs/projectiles/lazer_bolt.tscn")
 
 @onready var path_follow: PathFollow2D = self.get_parent()
 @onready var detection_area = $DetectionArea
+@onready var moving_timer = $MovingTimer
 
 var move_right: bool = true
 var state
 var target
-var bullet_speed = 1000
+var bullet_speed = 300
 
 func _ready():
 	state = StateEnum.MOVING
@@ -54,12 +55,14 @@ func attack():
 	lazer_bolt.position = global_position
 	lazer_bolt.look_at(target)
 	lazer_bolt.apply_impulse(Vector2(bullet_speed, 0).rotated(lazer_bolt.rotation), Vector2())
+	
 	state = StateEnum.IDLE
+	moving_timer.start()
 	
 func _on_detection_area_body_entered(body):
 	if body.name == "Player":
 		target = body.position
 		state = StateEnum.ATTACK
 
-func take_damage(damage):
-	pass
+func _on_moving_timer_timeout():
+	state = StateEnum.MOVING
--- scripts/projectiles/lazer_bolt.gd
+++ scripts/projectiles/lazer_bolt.gd
@@ -4,10 +4,13 @@ extends RigidBody2D
 
 @onready var hit_box = $HitBox
 
-var bullet_speed = 100
-
 func _ready():
 	hit_box.damage = damage
+	hit_box.monitorable = false
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+func _on_timer_timeout():
+	print("hello")
+	hit_box.monitorable = true
