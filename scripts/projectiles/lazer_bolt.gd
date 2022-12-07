fba95dfc5773b884e4cae860f244f5014b835035
Matthew Anthony Coker <mattacoker@gmail.com>
2022-12-06 19:52:19 -0500

    31 enemy ai flyer attack (#90)
    * drone attacks, has states, but explodes to its own bolts
    
    * enemy now shoots and doesnt die and switches states

---
+++ scripts/projectiles/lazer_bolt.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+@export var damage = 20
+
+@onready var hit_box = $HitBox
+
+func _ready():
+	hit_box.damage = damage
+	hit_box.monitorable = false
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+func _on_timer_timeout():
+	print("hello")
+	hit_box.monitorable = true
