3cf6d9eea994513970f7e67a955598cfac1ed9df
Matthew Anthony Coker <mattacoker@gmail.com>
2022-12-14 21:53:13 -0500

    31 enemy ai flyer attack (#93)
    * drone attacks, has states, but explodes to its own bolts
    
    * enemy now shoots and doesnt die and switches states
    
    * fix the tiles
    
    * some level edits
    
    * levels 1-7
    
    * updated more levels
    
    * player attack timer deleted
    
    * tileset
    
    * updated controls
    
    * updated portal
    
    * portals updated
    
    Co-authored-by: Synellx <synellx@gmail.com>

--- scripts/next_area.gd
+++ scripts/next_area.gd
@@ -5,4 +5,5 @@ var entered = false
 @export var level_select: String
 
 func _on_body_entered(body):
-	SceneTransition.change_scene_to_file("res://levels/" + level_folder +"/" + level_select + ".tscn")
+	if body.name == "Player":
+		SceneTransition.change_scene_to_file("res://levels/" + level_folder +"/" + level_select + ".tscn")
