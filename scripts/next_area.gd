c1facff63cc9887f90d7dd98f80eed578d65c097
Matthew Anthony Coker <mattacoker@gmail.com>
2022-12-14 20:30:20 -0500

    updated portal

--- scripts/next_area.gd
+++ scripts/next_area.gd
@@ -1,21 +1,8 @@
 extends Area2D
 
 var entered = false
+@export var level_folder: String
+@export var level_select: String
 
 func _on_body_entered(body):
-	print("entered")
-	entered = true
-	
-
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if entered == true:
-		if Input.is_action_just_pressed("Door"):
-			SceneTransition.change_scene_to_file("res://levels/Level_1/level_1.2.tscn")
-
-
-
+	SceneTransition.change_scene_to_file("res://levels/" + level_folder +"/" + level_select + ".tscn")
